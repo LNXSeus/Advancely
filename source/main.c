63111cb71aaae398fbfdd4987d4ad8981ed66487
Linus <lolnibob384@gmail.com>
2025-06-24 17:45:44 +0200

    first commit

---
+++ source/main.c
@@ -0,0 +1,22 @@
+//
+// Created by Linus on 24.06.2025.
+//
+
+#include "main.h"
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
+
+#include <SDL3_image/SDL_image.h>
+#include <SDL3_ttf/SDL_ttf.h>
+
+int main(int argc, char *argv[]) {
+    bool exit_status = EXIT_FAILURE;
+
+
+    exit_status = EXIT_SUCCESS;
+    return exit_status;
+}
---
+++ source/main.h
@@ -0,0 +1,8 @@
+//
+// Created by Linus on 24.06.2025.
+//
+
+#ifndef MAIN_H
+#define MAIN_H
+
+#endif //MAIN_H
