3de8674c8046a828cc0f3c13bfab9ac994386f56
Linus <lolnibob384@gmail.com>
2025-06-24 23:08:02 +0200

    Moved files around

--- source/main.c
+++ source/main.c
@@ -3,18 +3,14 @@
 //
 
 #include "main.h"
-#include <stdbool.h>
-#include <stdio.h>
-#include <stdlib.h>
 
-#include <SDL3/SDL.h>
-#include <SDL3/SDL_main.h>
-
-#include <SDL3_image/SDL_image.h>
-#include <SDL3_ttf/SDL_ttf.h>
 
 int main(int argc, char *argv[]) {
     bool exit_status = EXIT_FAILURE;
+
+
+
+
 
 
     exit_status = EXIT_SUCCESS;
--- source/main.h
+++ source/main.h
@@ -5,4 +5,17 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include "init.h"
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
+#include <SDL3_image/SDL_image.h>
+#include <SDL3_ttf/SDL_ttf.h>
+
+#include <cJSON.h>
+
 #endif //MAIN_H
